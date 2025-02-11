04a71559b080583415e083893ba21736d35e0bdb
elenee229 <ivechiani.elene@gmail.com>
2025-02-11 18:00:39 +0400

    commit

---
+++ day 01/homework/lesson01.py
@@ -0,0 +1,18 @@
+
+
+
+surname = "ivechiani"
+
+# print(name)
+
+
+
+name = "elene"
+age = 15
+height = 174
+
+
+loves_volleyball = True
+is_male = False
+
+print("me var" + " " + (name) + " " + "chemi gvaria" + " " + (surname) + "," + "var" + " " + str(age) + " " + "wlis" + " " + "chemi simaghlea" + " " + str(height) + "cm")
