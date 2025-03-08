135d1ed46102678d3e235989ead3e272096f0c9e
elenee229 <ivechiani.elene@gmail.com>
2025-03-08 14:33:25 +0400

    commit

---
+++ day 06/classwork/task01.py
@@ -0,0 +1,18 @@
+#შექმენით mybalance-ის ცვლადი, შემდეგ გაზარდეთ ამ ცვლადის მნიშვნელობა 2-თ უბრალო ფორმით, შემდეგ ისევ გაზარდეთ მნიშვნელობა 3-ით ოღონდ გამოიყენეთ assigment operator, ამის შემდეგ თქვენი სურვილისამებრ შეცვალეთ ცვლადის მნიშვნელობა იგივე assingment operator-ის საშვალებით, კოდი ახსენით კომენატრებით, ჩამოწერეთ ყველა საუკეტესო პრაქტიკა და მიუწერეთ განმარტებები
+
+my_balance = 20
+#increased by 3
+my_balance =my_balance + 3
+
+#increased by 3 with assigment operator
+my_balance += 3
+
+print(my_balance)
+
+#increased by 6 with assigment operator
+my_balance += 6
+print(my_balance)
+
+
+
+
