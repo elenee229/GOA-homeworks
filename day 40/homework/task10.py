45538660618261512403904a137c881039adbb58
elenee229 <ivechiani.elene@gmail.com>
2025-07-30 22:26:34 +0400

    commit

---
+++ day 40/homework/task10.py
@@ -0,0 +1,30 @@
+n1="me var elene"
+print(n1.split())
+n2="1 2 3 4 5 6 7 8 9 0"
+print(n2.split())
+n3="rogor xar megobaro"
+print(n3.split())
+
+n4=["hello","world"]
+n5=" ".join(n4)
+print(n5)
+
+n6=["2","4","7"]
+n7=" ".join(n6)
+print(n7)
+
+n8=["red","purple","white"]
+n9=" ".join(n8)
+print(n9)
+
+n0="i care about you"
+n01=n0.replace("you","me")
+print(n01)
+
+n02="white purple yellow"
+n03=n02.replace("yellow","red")
+print(n03)
+
+n04="1 4 3"
+n05=n04.replace("4","2")
+print(n05)
