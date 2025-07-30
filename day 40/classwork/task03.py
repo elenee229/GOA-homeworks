dc7d537e5b42d79b18c71b537a769988474db8bb
elenee229 <ivechiani.elene@gmail.com>
2025-07-30 21:44:43 +0400

    commit

---
+++ day 40/classwork/task03.py
@@ -0,0 +1,12 @@
+name="me mqvia elene"
+print(name.split())
+numbers="1 2 3 4 5 6"
+print(numbers.split())
+
+list=['hello','world']
+list1=" w".join(list)
+print(list1)
+
+n1="how are you"
+n2=n1.replace('how',"who")
+print(n2)
